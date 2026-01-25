79517856a7eb2376c662fb9059d66080353c782d
Your Name <yakir.israel1@gmail.com>
2026-01-25 19:57:23 +0200

    update souls function in HUD

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,9 +7,29 @@ public class HUD : MonoBehaviour
 {
     public TMP_Text healthText;
     public Slider healthBar;
+    
+    public Image soulsImage;
+    
+    public int souls = 5;
+
+    private float SoulsImageWidth = 150.0f;
+
+    private void Awake()
+    {
+        UpdateSouls(souls);
+    }
+
     public void UpdateHealthText(int health)
     {
         healthText.text = "Health: " + health;
+    }
+
+    public void UpdateSouls(int soulsAmount)
+    {
+        float x = SoulsImageWidth * soulsAmount;
+        float y = soulsImage.rectTransform.sizeDelta.y;
+        
+        soulsImage.rectTransform.sizeDelta = new Vector2(x, y);
     }
 
 
